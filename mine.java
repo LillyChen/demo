2da9054b755c955c2184f3242bdb0a8170789a73
chenhn <chnyjyx@163.com>
2018-07-15 00:37:09 +0800

    mine

--- mine.java
+++ mine.java
@@ -1,1 +1,12 @@
-wrttyetiq	twuetreytwtyw buhuhihuihuihu
+/**me*/
+public class Mine{
+	//单例模式
+	private static Singleton uniqueInstance;
+	private Singleton(){}
+	public static Singleton getInstance(){
+		if(uniqueInstance==null){
+			uniqueInstance=new Singleton();
+		}
+		return uniqueInstance;
+	}
+}
